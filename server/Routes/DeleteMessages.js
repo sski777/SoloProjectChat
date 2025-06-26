adef5c08339cb0d80bb8466a0a280ea516b5d642
Sski777 <david.karevin@gmail.com>
2025-06-26 13:06:37 +0300

    Delete Messages Uses ID

--- server/Routes/DeleteMessages.js
+++ server/Routes/DeleteMessages.js
@@ -1,26 +1,25 @@
 import pool from "../db.js";
-import dotenv from 'dotenv'
-import express from 'express'
+import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
 const route = express.Router();
 
-route.use(express.json())
+route.use(express.json());
 
+route.post("/", async (req, res) => {
+  try {
+    const gettitle = req.body.content;
+    const deleteentry = await pool.query(
+      "DELETE FROM messages WHERE id = $1 RETURNING *",
+      [gettitle]
+    );
+    const gettable = await pool.query("SELECT * FROM messages");
+    res.status(201).json(gettable.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-route.post('/', async (req, res) => {
-  try{
-    const gettitle = req.body.content
-    const deleteentry = await pool.query("DELETE FROM messages WHERE content = $1 RETURNING *", [gettitle])
-    const gettable = await pool.query('SELECT * FROM messages')
-    res.status(201).json(gettable.rows)
-  } 
-  catch(error){
-    res.status(400).json({error:error.message})
-  } 
-})
-
-
-
-export default route
+export default route;
